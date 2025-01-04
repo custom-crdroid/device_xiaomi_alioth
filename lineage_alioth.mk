fed0f7f2fe5cca58b98cf602660994b43b37779e
althafvly <althafvly@gmail.com>
2025-01-04 22:48:27 +0530

    alioth: Update blobs from OS1.0.3.0.TKHMIXM
    Change-Id: I51968565254d2a913181fdf0c9dd52a6617b913d

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -26,7 +26,7 @@ PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.2.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.3.0.TKHMIXM:user/release-keys
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
@@ -45,3 +45,4 @@ TARGET_SUPPORTS_QUICK_TAP := true
 
 #Bypass charging
 BYPASS_CHARGE_SUPPORTED := true
+
