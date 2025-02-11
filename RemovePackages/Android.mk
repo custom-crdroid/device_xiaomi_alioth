7fddd73765a4cc8c4db8aa952221e50588957f6b
darkphnx <dark.phnx12@gmail.com>
2025-02-11 19:29:29 +0530

    update remove package
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -19,12 +19,9 @@ LOCAL_OVERRIDES_PACKAGES := \
     Chrome-Stub \
     Drive \
     DeskClock \
-    stk \
-    Stk \
     GoogleCamera \
     Glimpse \
     Gallery2 \
-    FilesPrebuilt \
     MiuiScanner \
     Matlog \
     MatLog \
